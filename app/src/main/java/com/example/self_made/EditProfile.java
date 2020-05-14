7ca7c831baba8157015d50b041f58d781a411d6d
kassimii <patyboc@yahoo.com>
2020-05-15 00:56:14 +0300

    Conected profile info to db

--- app/src/main/java/com/example/self_made/EditProfile.java
+++ app/src/main/java/com/example/self_made/EditProfile.java
@@ -7,16 +7,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class EditProfile extends AppCompatActivity {
 
-    private Button setMealsButton, analysePhotoButton;
+    private Button setMealsButton, analysePhotoButton, saveProfileButton;
+
+    private EditText ageInput, heightInput, weightInput;
+    private ImageButton femaleButton, maleButton, eggsButton,diaryButton, fishButton, glutenButton, peanutButton;
+
+    private String age, height, weight;
+    private boolean isFemale, isMale, isAllergicToEggs=false, isAllergicToDiary=false, isAllergicToFish=false, isAllergicToGluten=false, isAllergicToPeanut=false;
+    private String allergies="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         onMealButtonClick();
         onCameraButtonClick();
+        onSaveProfileButtonClick();
     }
 
     public void onMealButtonClick()
@@ -61,5 +75,125 @@ public class EditProfile extends AppCompatActivity {
 
         overridePendingTransition(0,0);
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+    }
+
+    public void onSaveProfileButtonClick(){
+        ageInput = (EditText)findViewById(R.id.age_input);
+        heightInput = (EditText)findViewById(R.id.height_input);
+        weightInput = (EditText)findViewById(R.id.weight_input);
+
+        femaleButton = (ImageButton)findViewById(R.id.female_button);
+        femaleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isFemale=true;
+                isMale=false;
+            }
+        });
+
+        maleButton = (ImageButton)findViewById(R.id.male_button);
+        maleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isFemale=false;
+                isMale=true;
+            }
+        });
+
+        eggsButton = (ImageButton)findViewById(R.id.eggs_button);
+        eggsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAllergicToEggs=true;
+            }
+        });
+
+        diaryButton = (ImageButton)findViewById(R.id.dairy_button);
+        diaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAllergicToDiary=true;
+            }
+        });
+
+        fishButton = (ImageButton)findViewById(R.id.fish_button);
+        fishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAllergicToFish=true;
+            }
+        });
+
+        glutenButton = (ImageButton)findViewById(R.id.gluten_button);
+        glutenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAllergicToGluten=true;
+            }
+        });
+
+        peanutButton = (ImageButton)findViewById(R.id.peanut_button);
+        peanutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAllergicToPeanut=true;
+            }
+        });
+
+
+
+        saveProfileButton = (Button)findViewById(R.id.save_profile_button);
+        saveProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                age = ageInput.getText().toString();
+                height = heightInput.getText().toString();
+                weight = weightInput.getText().toString();
+
+                if(age.length()!=0)
+                    {FirebaseDatabase.getInstance().getReference().child("Profile").child("Age").setValue(age);}
+                if(height.length()!=0)
+                   { FirebaseDatabase.getInstance().getReference().child("Profile").child("Height").setValue(height);}
+                if(weight.length()!=0)
+                    {FirebaseDatabase.getInstance().getReference().child("Profile").child("Weight").setValue(weight);}
+
+                if(isMale==true){
+                    FirebaseDatabase.getInstance().getReference().child("Profile").child("Gender").setValue("male");
+                }
+
+                if(isFemale==true){
+                    FirebaseDatabase.getInstance().getReference().child("Profile").child("Gender").setValue("female");
+                }
+
+                if(isAllergicToEggs==true){
+                    allergies+="eggs ";
+                }
+
+                if(isAllergicToDiary==true){
+                    allergies+="diary ";
+                }
+
+                if(isAllergicToFish==true){
+                    allergies+="fish ";
+                }
+
+                if(isAllergicToGluten==true){
+                    allergies+="gluten ";
+                }
+
+                if(isAllergicToPeanut==true){
+                    allergies+="peanut ";
+                }
+
+                if(allergies.length()!=0){
+                    FirebaseDatabase.getInstance().getReference().child("Profile").child("Allergies").setValue(allergies);
+                    allergies="";
+                }
+
+
+            }
+        });
+
+
     }
 }
